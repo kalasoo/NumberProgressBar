1e87fa075b00340e82e99aa1c39913d77a988c0b
kalasoo <ym.kalasoo@gmail.com>
2014-05-03 00:38:00 +0100

    polish details

--- number-pb.js
+++ number-pb.js
@@ -8,6 +8,7 @@
     }, options || {});
 
     this.duration = settings.duration;
+    this.last_percentage = -1;
     this.percentage = (settings.percentage >= 0 && settings.percentage <= 100) ? settings.percentage : 0;
     this.$element = $(element);
     this.width = this.$element.width();
@@ -18,6 +19,12 @@
   }
 
   NumberProgressBar.prototype.reach = function(percentage) {
+    if (this.last_percentage < 0) {
+      this.last_percentage = 0;
+    } else {
+      this.last_percentage = this.percentage;  
+    }
+    
     if (percentage < 0) {
       this.percentage = 0;
     } else if (percentage > 100) {
@@ -25,9 +32,13 @@
     } else {
       this.percentage = percentage;
     }
-    console.log('reach: ', this.percentage);
+    console.log('reach: ', this.last_percentage, this.percentage, this.calDuration());
     this.moveShown();
     this.moveNum();
+  }
+
+  NumberProgressBar.prototype.calDuration = function() {
+    return this.duration * Math.abs(this.percentage - this.last_percentage) / 100.0;
   }
 
   NumberProgressBar.prototype.moveShown = function() {
@@ -35,39 +46,40 @@
     this.$shownBar.velocity({
       width: this.percentage + '%'
     }, {
-      duration: this.duration
+      duration: this.calDuration()
     })
   }
 
   NumberProgressBar.prototype.moveNum = function() {
     console.log('moveNum: ', this.percentage);
-        var self = this;
-        var left = this.width * this.percentage / 100.0;
-        var numWidth = this.$num.width();
-        if (numWidth + left > this.width) {
-          var percentage = (this.width - numWidth) / this.width * 100.0;
-        } else {
-          var percentage = this.percentage;
-        }
+    var self = this;
+    var left = this.width * this.percentage / 100.0;
+    var numWidth = this.$num.width();
+    if (numWidth + left > this.width) {
+      var percentage = (this.width - numWidth) / this.width * 100.0;
+    } else {
+      var percentage = this.percentage;
+    }
 
-        this.$num.velocity({
-          left: percentage + '%'
-        }, {
-          duration: this.duration
-        })
+    this.$num.velocity({
+      left: percentage + '%'
+    }, {
+      duration: this.calDuration()
+    })
 
-        // number
-        $({num: parseInt(this.$num.text())}).animate({
-          num: this.percentage
-        }, {
-          duration: self.duration,
-          step: function() {
-            self.$num.text(Math.ceil(this.num) + '%');
-          },
-          complete: function() {
-            self.$num.text(self.percentage + '%');
-          }
-        })
+    // number
+    $({num: parseInt(this.$num.text())}).animate({
+      num: this.percentage
+    }, {
+      queue: true,
+      duration: self.calDuration(),
+      step: function() {
+        self.$num.text(Math.ceil(this.num) + '%');
+      },
+      complete: function() {
+        self.$num.text(self.percentage + '%');
+      }
+    })
   }
 
   $.fn.NumberProgressBar = function(options) {
